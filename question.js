d75e09083756ad8497f3bd236c5b901796b14821
Pjatte <91551936+Pjatte1337@users.noreply.github.com>
2022-07-06 14:31:09 +0200

    quiz update

---
+++ question.js
@@ -0,0 +1,57 @@
+const question = [
+    {
+        number:1,
+        question:'What does HTML stand for?',
+        answer: ' hyper text markup languagge',
+        options: [
+            'Home tool markup languagge',
+            'hyper text markup languagge',
+            'hyperlinks and text markup languagge',
+            'hyper tool markup languagge'
+        ]
+    },
+    {
+        number:2,
+        question:'What does HTML stand for?',
+        answer: ' hyper text markup languagge',
+        options: [
+            'Home tool markup languagge',
+            'hyper text markup languagge',
+            'hyperlinks and text markup languagge',
+            'hyper tool markup languagge'
+        ]
+    },
+    {
+        number:3,
+        question:'What does HTML stand for?',
+        answer: ' hyper text markup languagge',
+        options: [
+            'Home tool markup languagge',
+            'hyper text markup languagge',
+            'hyperlinks and text markup languagge',
+            'hyper tool markup languagge'
+        ]
+    },
+    {
+        number:4,
+        question:'What does HTML stand for?',
+        answer: ' hyper text markup languagge',
+        options: [
+            'Home tool markup languagge',
+            'hyper text markup languagge',
+            'hyperlinks and text markup languagge',
+            'hyper tool markup languagge'
+        ]
+    },
+    {
+        number:5,
+        question:'What does HTML stand for?',
+        answer: ' hyper text markup languagge',
+        options: [
+            'Home tool markup languagge',
+            'hyper text markup languagge',
+            'hyperlinks and text markup languagge',
+            'hyper tool markup languagge'
+        ]
+    },
+]
