819f47d4be91bb25ae85850d9fa35506af1855b7
Pjatte <91551936+Pjatte1337@users.noreply.github.com>
2022-07-07 03:47:21 +0200

    update

--- question.js
+++ question.js
@@ -1,57 +1,90 @@
-const question = [
+const questions = [
     {
-        number:1,
-        question:'What does HTML stand for?',
-        answer: ' hyper text markup languagge',
-        options: [
-            'Home tool markup languagge',
-            'hyper text markup languagge',
-            'hyperlinks and text markup languagge',
-            'hyper tool markup languagge'
-        ]
-    },
+  number:1,        
+  question:'What does HTML Stand for?', 
+  answer:'Hyper Text Markup Languagge', 
+  options:[
+          'Home Tool Markup Laguagge', 
+          'Hyper Text Markup Languagge', 
+          'Hyperlinks and Text Markup Languagge', 
+          'Hyper Tool Markup Laguagge'
+          ]
+    }, 
     {
-        number:2,
-        question:'What does HTML stand for?',
-        answer: ' hyper text markup languagge',
-        options: [
-            'Home tool markup languagge',
-            'hyper text markup languagge',
-            'hyperlinks and text markup languagge',
-            'hyper tool markup languagge'
-        ]
-    },
+            number:2,
+            question: 'What does CSS stand for?',
+            answer: 'Cascading Style Sheets',
+            options: [
+          'Computer Style Sheets',
+          'Creative Style Sheets',
+          'Colorful Style Sheets',
+          'Cascading Style Sheets'
+          ]
+    }, 
     {
-        number:3,
-        question:'What does HTML stand for?',
-        answer: ' hyper text markup languagge',
-        options: [
-            'Home tool markup languagge',
-            'hyper text markup languagge',
-            'hyperlinks and text markup languagge',
-            'hyper tool markup languagge'
-        ]
-    },
+            number:3,
+            question: 'What does PHP stand for?',
+            answer: 'Hypertext Preprocessor',
+            options: [
+          'Preprocessed Hypertext Page',
+          'Hypertext Preprocessor',
+          'Processor Hypertext Processor',
+          'None of the above'
+          ]
+    }, 
     {
-        number:4,
-        question:'What does HTML stand for?',
-        answer: ' hyper text markup languagge',
-        options: [
-            'Home tool markup languagge',
-            'hyper text markup languagge',
-            'hyperlinks and text markup languagge',
-            'hyper tool markup languagge'
-        ]
-    },
+            number:4,
+            question: 'Which of the below CSS class is used to change the text color of CSS ?',
+            answer: 'color',
+            options: [
+          'background-color',
+          'color-background',
+          'color',
+          'None of the above'
+          ]
+    }, 
     {
-        number:5,
-        question:'What does HTML stand for?',
-        answer: ' hyper text markup languagge',
-        options: [
-            'Home tool markup languagge',
-            'hyper text markup languagge',
-            'hyperlinks and text markup languagge',
-            'hyper tool markup languagge'
-        ]
-    },
-]
+            number:5,
+            question:'Which of the below is the correct syntax to put a line over text in CSS?',
+            answer:'text-decoration: overline',
+            options:[
+          'text-decoration: line',
+          'text-decoration: none',
+          'text-decoration: overline',
+          'text-decoration: underline'
+          ]
+    }, 
+    {
+        number:6,
+        question:'Which of the below is the correct syntax to put a line over text in CSS?',
+        answer:'text-decoration: overline',
+        options:[
+      'text-decoration: line',
+      'text-decoration: none',
+      'text-decoration: overline',
+      'text-decoration: underline'
+      ]
+}, 
+{
+    number:7,
+    question:'Which of the below is the correct syntax to put a line over text in CSS?',
+    answer:'text-decoration: overline',
+    options:[
+  'text-decoration: line',
+  'text-decoration: none',
+  'text-decoration: overline',
+  'text-decoration: underline'
+  ]
+}, 
+{
+    number:8,
+    question:'Which of the below is the correct syntax to put a line over text in CSS?',
+    answer:'text-decoration: overline',
+    options:[
+  'text-decoration: line',
+  'text-decoration: none',
+  'text-decoration: overline',
+  'text-decoration: underline'
+  ]
+}, 
+ ]
